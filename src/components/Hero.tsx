a1d8e76527ea6c9c2484dfe0a67e0f9359e97b5a
hanbitk <hanbitkang95@gmail.com>
2024-04-04 22:06:07 +0900

    created hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="h-screen bg-hero bg-cover bg-no-repeat flex flex-col justify-center items-center text-white text-center">
+      <h1 className="w-1/2 leading-[5rem] font-bold text-7xl">
+        Methane Minimized, World Maximixed.
+      </h1>
+      <h2 className="mt-2">
+        With MeTech, reduce methane emissions by 40% from over 1.5 billion cows
+        worldwide.
+      </h2>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,11 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 function Home() {
   return (
-    <div className="h-screen bg-hero bg-cover bg-no-repeat flex flex-col justify-center items-center text-white text-center">
-      <h1 className="w-1/2 leading-[5rem] font-bold text-7xl">
-        Methane Minimized, World Maximixed.
-      </h1>
-      <h2 className="mt-2">
-        With MeTech, reduce methane emissions by 40% from over 1.5 billion cows
-        worldwide.
-      </h2>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
 
