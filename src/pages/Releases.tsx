ab6a5f209091354b2a81f4f6f8ac8c29888d9348
hanbitk <hanbitkang95@gmail.com>
2024-04-16 18:37:38 +0900

    fix: contact

--- src/pages/Releases.tsx
+++ src/pages/Releases.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Releases() {
-  return <div>Releases</div>;
+  return (
+    <main>
+      {/* Releases Header */}
+      <div className="w-full h-[320px] bg-offWhite pt-32 p-20">
+        <h1 className="text-6xl text-darkGreen font-extrabold mb-10">
+          News and updates from MeTech
+        </h1>
+        <div className="w-[200px] h-[8px] bg-lightGreen rounded-sm"></div>
+      </div>
+    </main>
+  );
 }
 
 export default Releases;
