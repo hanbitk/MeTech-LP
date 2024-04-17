82b50eceaaf97f605e2f6e1f53146b72f5d58984
hanbitk <hanbitkang95@gmail.com>
2024-04-17 13:18:16 +0900

    video autoplay attribute

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -9,7 +9,7 @@ function YoutubeEmbed({ embedId }: IYoutubeEmbedProps) {
     <iframe
       width="460"
       height="250"
-      src={`https://www.youtube.com/embed/${embedId}`}
+      src={`https://www.youtube.com/embed/${embedId}?autoplay=1&mute=1`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
